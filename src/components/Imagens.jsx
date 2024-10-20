088952e94dca0c6499c6fedd6f4643ace80a4149
felipin <felipinhobgouveia@gmail.com>
2024-10-20 19:10:35 -0300

    nao

--- src/components/Imagens.jsx
+++ src/components/Imagens.jsx
@@ -1,18 +1,28 @@
-function Imagem(props){
-    return <img src={props.url} width={'10%'} style={{borderRadius: '50%', border: '3px solid #000'}}/>
+import "./Imagens.css";
+
+function Imagem(props) {
+  return (
+      <img
+        src={props.url}
+        width={"100px"}
+        style={{ borderRadius: "50%", border: "3px solid #000" }}
+      />
+  );
 }
 
-function Imagens(){
-    const urls = [
-        'https://yt3.googleusercontent.com/LEI4pyLBybLFNag3c57eJ2mtPesFOVhtLeaawJzsVLoU6Cnp58CSVohjhlAQqLw8nRhPiK6qGg=s900-c-k-c0x00ffffff-no-rj',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOdcOOzrBHvl9xUxTpuCfXUD5WuzLxeC_Ylw&s'
-    ]
+function Imagens() {
+  const urls = [
+    "https://yt3.googleusercontent.com/LEI4pyLBybLFNag3c57eJ2mtPesFOVhtLeaawJzsVLoU6Cnp58CSVohjhlAQqLw8nRhPiK6qGg=s900-c-k-c0x00ffffff-no-rj",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOdcOOzrBHvl9xUxTpuCfXUD5WuzLxeC_Ylw&s",
+  ];
 
-    return(
-        <>
-        {urls.map((urls) => <Imagem url={urls} />)}
-        </>
-    )
+  return (
+    <div className="fotos">
+          {urls.map((urls) => (
+            <Imagem url={urls} />
+          ))}
+    </div>
+  );
 }
 
-export default Imagens
+export default Imagens;
